b894209ac27afc9a1ceace8fe287e0cfd19fd2ea
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-08-30 13:09:58 +0400

    add forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,17 @@
 class Post
+  extend ActiveModel::Naming
   include Virtus.model
 
   attribute :id, String, writer: :private, default: -> (_post, _attr) { SecureRandom.uuid }
   attribute :title, String
   attribute :content, String
   attribute :published_at, DateTime, default: -> (_post, _attr) { DateTime.current }
+
+  def to_key
+    [id]
+  end
+
+  def to_param
+    id
+  end
 end
