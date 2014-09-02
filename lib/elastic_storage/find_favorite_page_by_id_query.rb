12748c140b8a57c3ae9e7932ffe6bfe3ebe3c9d8
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-09-02 22:24:51 +0400

    reafactoring

--- lib/elastic_storage/find_favorite_page_by_id_query.rb
+++ lib/elastic_storage/find_favorite_page_by_id_query.rb
@@ -1,16 +1,11 @@
 module ElasticStorage
   module FindFavoritePageByIdQuery
-    extend Client
     extend self
 
     def call(id, silent: false )
-      params = { index: 'documents', type: 'favorite_page', id: id }
-      params.merge! ignore: [404] if silent
-      responce = client.get params
+      responce = FindByIdQuery.call id, :favorite_page, silent: silent
       return unless responce
       FavoritePageMapper.from_responce id, responce['_source']
-    rescue Elasticsearch::Transport::Transport::Errors::NotFound
-      raise NotFound
     end
   end
 end
--- lib/elastic_storage/find_post_by_id_query.rb
+++ lib/elastic_storage/find_post_by_id_query.rb
@@ -1,16 +1,11 @@
 module ElasticStorage
   module FindPostByIdQuery
-    extend Client
     extend self
 
     def call(id, silent: false )
-      params = { index: 'documents', type: 'post', id: id }
-      params.merge! ignore: [404] if silent
-      responce = client.get params
+      responce = FindByIdQuery.call id, :post, silent: silent
       return unless responce
       PostMapper.from_responce id, responce['_source']
-    rescue Elasticsearch::Transport::Transport::Errors::NotFound
-      raise NotFound
     end
   end
 end
