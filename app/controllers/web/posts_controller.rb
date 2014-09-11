1f89df6e98bb3a6151d8eecc4004f2c72cc772fa
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-09-11 10:13:01 +0400

    add similar query

--- app/controllers/web/posts_controller.rb
+++ app/controllers/web/posts_controller.rb
@@ -3,7 +3,7 @@ class Web::PostsController < Web::ApplicationController
 
   def show
     @post = storage.find_post_by_id_query.call params[:id]
-    @materials = storage.search_query.call @post.favorite_pages_query
+    @materials = storage.search_query.call @post.similar_query
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post
   attribute :id, String, writer: :private, default: -> (_post, _attr) { SecureRandom.uuid }
   attribute :title, String
   attribute :content, String
+  #TODO: rename this
   attribute :favorite_pages_query, String
   attribute :published_at, DateTime, default: -> (_post, _attr) { DateTime.current }
 
@@ -18,5 +19,9 @@ class Post
 
   def to_s
     title
+  end
+
+  def similar_query
+    "NOT (_id: #{id}) AND (#{favorite_pages_query})"
   end
 end
