f4d54de2f6f9364488fb26180124941b394ed243
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2014-09-18 16:21:41 +0400

    refactoring

--- app/read_models/preview.rb
+++ app/read_models/preview.rb
@@ -6,12 +6,6 @@ class Preview
   attribute :published_at, DateTime
   attribute :type
 
-  delegate :url_helpers, to: 'Rails.application.routes'
-
-  def path(opts={})
-    url_helpers.send "#{type}_path", id, opts
-  end
-
   def to_s
     title
   end
--- app/read_models/search_result.rb
+++ app/read_models/search_result.rb
@@ -7,12 +7,6 @@ class SearchResult
   attribute :highlighted_content
   attribute :type
 
-  delegate :url_helpers, to: 'Rails.application.routes'
-
-  def path(opts={})
-    url_helpers.send "#{type}_path", id, opts
-  end
-
   def to_s
     title
   end
