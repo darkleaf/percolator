01e6c67c0b5b70adb040bb4fab4bb7e6c32fef9f
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2015-04-05 23:23:59 +0300

    helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def markdown(content)
-    options = {
-      autolink: true,
-      fenced_code_blocks: true
-    }
-    markdown = Redcarpet::Markdown.new Renderer, options
-    markdown.render(content).html_safe
-  end
-
   def han(model, attribute)
     model.to_s.classify.constantize.human_attribute_name(attribute)
   end
